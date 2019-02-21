f9d9040b0f8bf03d562f651436b58da9a766c743
MTushabe <mtushabe@MacBooks-MacBook-Pro.local>
2019-02-21 12:34:01 +0300

    first-commit1

--- src/main/java/org/savics/challenge/models/Patient.java
+++ src/main/java/org/savics/challenge/models/Patient.java
@@ -43,4 +43,44 @@ public class Patient {
 		this.id = id;
 	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public Options getDiabetic() {
+		return diabetic;
+	}
+
+	public void setDiabetic(Options diabetic) {
+		this.diabetic = diabetic;
+	}
+
 }
